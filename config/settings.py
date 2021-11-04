5200e036156ee5754551ae897d1b55b4a2a53fa6
Marty0427 <93676663+Marty0427@users.noreply.github.com>
2021-11-04 17:56:05 +0100

    Add files via upload

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+
+# cesta k databazi
+setsql = {
+        'dbpath' : 'tradebook.db3'
+}
+
+# parametry pripojeni IB
+setIB = {
+        'IP' : '127.0.0.1',
+        'port' : 7497,
+        'clientID' : 5
+}
+
+
